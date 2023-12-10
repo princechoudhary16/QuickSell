fd2c83678ce33d927d9413af6a53552b2c91682e
princechoudhary16 <91569863+princechoudhary16@users.noreply.github.com>
2023-12-10 05:48:35 +0530

    Update Column.js

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,19 +1,4 @@
-// import React from 'react';
-// import Ticket from './Ticket';
-// import './styles/Column.css';
 
-// const Column = ({ title, tickets }) => {
-//   return (
-//     <div className="column">
-//       <h2>{title}</h2>
-//       {tickets.map((ticket) => (
-//         <Ticket key={ticket.id} ticket={ticket} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Column;
 import React from 'react';
 import { Droppable } from 'react-beautiful-dnd';
 import Ticket from './Ticket';
